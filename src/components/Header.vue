fccc3f65dc56c7d1a1b86b9898fea4fe53dd5c47
Adebimpe <adeadebimpe96@gmail.com>
2018-11-13 12:37:20 +0100

    added product items

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="header">
+    <div class="container">
+        <div class="header">
         <h3 class="header__title">Ash Luxury</h3>
-        <p>Fashion & Lifestyle</p>
+        <p class="header__sub-title">Fashion & Lifestyle 📧 admin@ashluxury.com 📲 </p>
+<p class="header__sub-title">whatsapp: +234 8169761216 🌍 worldwide shipping </p>
+<p class="header__sub-title">📍22B Admiralty Way 🕙HQ opens 10am - 8pm (mon-sat)</p>
+    </div>
     </div>
 </template>
 
@@ -12,6 +16,8 @@
     }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+    .header{
+        margin: 100px 0px;
+    }
 </style>
--- src/components/Items.vue
+++ src/components/Items.vue
@@ -1,27 +1,150 @@
 <template>
-    <div>
-        <div class="grid-container">
-  <div class="item1">1</div>
-  <div class="item2">2</div>
-  <div class="item3">3</div>  
-  <div class="item4">4</div>
-  <div class="item5">5</div>
-  <div class="item6">6</div>
-  <div class="item7">7</div>
-  <div class="item8">8</div>  
-  <div class="item9">9</div>
-  <div class="item10">10</div>
+  <div class="container">
+    <div class="product">
+      <div class="product__item product__item1">
+        <img src="../assets/t-shirt-1710578_1280.jpg" alt="T-shirt" class="product_image" style="width:100%">
+        <div class="product__item--text">
+          <h5>N120,000</h5>
+          <p>Available in size 12, 13, 14</p>
+          <button class="btn"> Buy now</button>
+        </div>
+      </div>
+      <div class="product__item product__item2">
+        <img src="../assets/t-shirt-1710578_1280.jpg" alt="T-shirt" class="product_image" style="width:100%">
+        <div class="product__item--text">
+          <h5>N120,000</h5>
+          <p>Available in size 12, 13, 14</p>
+          <button class="btn"> Buy now</button>
+        </div>
+      </div>
+      <div class="product__item product__item3">
+        <img src="../assets/t-shirt-1710578_1280.jpg" alt="T-shirt" class="product_image" style="width:100%">
+        <div class="product__item--text">
+          <h5>N120,000</h5>
+          <p>Available in size 12, 13, 14</p>
+          <button class="btn"> Buy now</button>
+        </div>
+      </div>
+      <div class="product__item product__item4">
+        <img src="../assets/t-shirt-1710578_1280.jpg" alt="T-shirt" class="product_image" style="width:100%">
+        <div class="product__item--text">
+          <h5>N120,000</h5>
+          <p>Available in size 12, 13, 14</p>
+          <button class="btn"> Buy now</button>
+        </div>
+      </div>
+      <div class="product__item product__item5">
+        <img src="../assets/chucks-153310_640.png" alt="T-shirt" class="product_image" style="width:50%">
+        <div class="product__item--text">
+          <h5>N120,000</h5>
+          <p>Available in size 12, 13, 14</p>
+          <button class="btn"> Buy now</button>
+        </div>
+      </div>
+      <div class="product__item product__item6">
+        <img src="../assets/chucks-153310_640.png" alt="T-shirt" class="product_image" style="width:50%">
+        <div class="product__item--text">
+          <h5>N120,000</h5>
+          <p>Available in size 12, 13, 14</p>
+          <button class="btn"> Buy now</button>
+        </div>
+      </div>
+      <div class="product__item product__item7">
+        <img src="../assets/t-shirt-1710578_1280.jpg" alt="T-shirt" class="product_image" style="width:100%">
+        <div class="product__item--text">
+          <h5>N120,000</h5>
+          <p>Available in size 12, 13, 14</p>
+          <button class="btn"> Buy now</button>
+        </div>
+      </div>
+      <div class="product__item product__item8">
+        <img src="../assets/t-shirt-1710578_1280.jpg" alt="T-shirt" class="product_image" style="width:100%">
+        <div class="product__item--text">
+          <h5>N120,000</h5>
+          <p>Available in size 12, 13, 14</p>
+          <button class="btn"> Buy now</button>
+        </div>
+      </div>
+      <div class="product__item product__item9">
+        <img src="../assets/t-shirt-1710578_1280.jpg" alt="T-shirt" class="product_image" style="width:100%">
+        <div class="product__item--text">
+          <h5>N120,000</h5>
+          <p>Available in size 12, 13, 14</p>
+          <button class="btn"> Buy now</button>
+        </div>
+      </div>
+      <div class="product__item product__item10">
+        <img src="../assets/t-shirt-1710578_1280.jpg" alt="T-shirt" class="product_image" style="width:100%">
+        <div class="product__item--text">
+          <h5>N120,000</h5>
+          <p>Available in size 12, 13, 14</p>
+          <button class="btn"> Buy now</button>
+        </div>
+      </div>
 
-</div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+  export default {
+
+  }
+
 </script>
 
-<style scoped>
+<style lang="scss">
+  .product__item {
+    position: relative;
+
+    &:hover {
+      .product_image {
+        opacity: 0.3;
+      }
+
+      .product__item--text {
+        opacity: 1;
+      }
+
+    }
+
+  }
+
+  .product_image {
+    opacity: 1;
+    display: block;
+    width: 100%;
+    height: auto;
+    transition: .5s ease;
+    backface-visibility: hidden;
+  }
+
+  .product__item--text {
+    transition: .5s ease;
+    opacity: 0;
+    position: absolute;
+    top: 20%;
+    left: 20%;
+
+    h5 {
+
+      font-size: 18px;
+      font-weight: 500;
+      text-align: left;
+      line-height: 2;
+      color: #fff;
+    }
+
+    p {
+      font-size: 14px;
+      letter-spacing: 0px;
+      text-align: left;
+      color: #ffffff;
+    }
+
+    button {
+      margin-top: 100px;
+    }
+  }
 
 </style>
