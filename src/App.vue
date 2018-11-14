37b0952cd0322d5a6e67824bb5591cef66af2bc0
Adebimpe <adeadebimpe96@gmail.com>
2018-11-14 15:04:09 +0100

    changed images for app

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,94 @@
 <template>
   <div id="app">
 
-    <div class="container">
+    <div class="wrapper">
 
-  
+
 
       <div v-if="!isShowingCart" id="products">
         <div class="header">
-          <nav id="top-navigation" class="well">
-            <div class="cart-icon" @click="isShowingCart = true">
+          <div class="container">
+            <nav id="top-navigation" class="well">
+              <div class="cart-icon" @click="isShowingCart = true">
 
-              <a><img src="../src/assets/bag.svg" alt="Cart Bag" width="100%" />
-              <span class="cart-item">{{cart.items.length }}</span></a>
+                <a><img src="../src/assets/bag.svg" alt="Cart Bag" width="100%" />
+                  <span class="cart-item">{{cart.items.length }}</span></a>
 
 
+              </div>
+            </nav>
+            <div class="header__text">
+
+              <h3 class="header__title">Ash Luxury</h3>
+              <p class="header__sub-title">Fashion & Lifestyle 📧 admin@ashluxury.com 📲 </p>
+              <p class="header__sub-title">whatsapp: +234 8169761216 🌍 worldwide shipping </p>
+              <p class="header__sub-title">📍22B Admiralty Way 🕙HQ opens 10am - 8pm (mon-sat)</p>
             </div>
-          </nav>
-          <div class="header__text">
-
-            <h3 class="header__title">Ash Luxury</h3>
-            <p class="header__sub-title">Fashion & Lifestyle 📧 admin@ashluxury.com 📲 </p>
-            <p class="header__sub-title">whatsapp: +234 8169761216 🌍 worldwide shipping </p>
-            <p class="header__sub-title">📍22B Admiralty Way 🕙HQ opens 10am - 8pm (mon-sat)</p>
           </div>
 
         </div>
 
         <div class="product">
-          <div v-for="product in products" :key="product.id" :class="['product__item' + product.id, 'product__item' ]">
-            <img :src="product.avatar" class="product_image" width="100%" alt="Product Image" />
-            <div class="product__item--text">
-              <h5>{{product.price | currency }}</h5>
-              <p>{{product.description}}</p>
-              <button class="btn btn-white" @click="addProductToCart(product)"> Add to Cart</button>
-              <button class="btn btn-white" @click="removeProductToCart(product)"> Remove from Cart</button>
-            </div>
+          <div class="container">
+            <div v-for="product in products" :key="product.id" :class="['product__item' + product.id, 'product__item' ]"
+              @click="showModal = true">
+              <img :src="product.avatar" class="product_image" width="100%" alt="Product Image" />
+              <div class="product__item--text">
+                <h5>{{product.price | currency }}</h5>
+                <p>{{product.description}}</p>
+                <button class="btn btn-white" @click="addProductToCart(product)" v-once> Add to Cart</button>
+                <button class="btn btn-danger" @click="removeProductFromCart(product)"> Remove from Cart</button>
 
+              </div>
+
+            </div>
           </div>
         </div>
         <footer>
-          <div class="wrapper">
-            <a class="btn btn-black" href="#">Follow Us on Instagram</a>
-          </div>
+          
+            <div class="wrapper">
+              <a class="btn btn-black" href="#">Follow Us on Instagram</a>
+            </div>
+          
         </footer>
       </div>
       <div v-else>
         <h1 class="cart-header">Your Cart</h1>
-        <div class="cart" v-if="cart.items.length > 0">
+        <div class="container">
           
-          <div  v-for="item in cart.items" :key="item.product.id" class="cart-item row">
-            <div class="col-md-3">
-              <img :src="item.product.avatar" alt="Bag" width="100%" />
-            </div>
-            <div class="col-md-6 cart-info">
-              <h5 class="item-name"> {{item.product.name}}</h5>
-              <p class="item-description"> {{ item.product.description }}</p>
-              <p class="item-price">{{item.quantity * item.product.price | currency}}</p>
-            </div>
-            <div class="col-md-3 cart-no">
+          <div class="cart" v-if="cart.items.length > 0">
 
-              <button class="btn btn-black" @click="increaseQuantity(item)">+</button>
-              <p> {{ item.quantity }} </p>
-              <button class="btn btn-black" @click="decreaseQuantity(item)">-</button>
+            <div v-for="item in cart.items" :key="item.product.id" class="cart-item row">
+              <div class="col-md-3">
+                <img :src="item.product.avatar" alt="Bag" width="100%" />
+              </div>
+              <div class="col-md-6 cart-info">
+                <h5 class="item-name"> {{item.product.name}}</h5>
+                <p class="item-description"> {{ item.product.description }}</p>
+                <p class="item-price">{{item.quantity * item.product.price | currency}}</p>
+              </div>
+              <div class="col-md-3 cart-no">
+
+                <button class="btn btn-black" @click="increaseQuantity(item)">+</button>
+                <p> {{ item.quantity }} </p>
+                <button class="btn btn-black" @click="decreaseQuantity(item)">-</button>
+              </div>
+              <!--<div class="cart-remove">
+              <button >Remove</button>
+            </div>-->
             </div>
-            <div class="cart-remove">
-              <p>Remove</p>
+            <hr>
+            <div class="cart-total">
+              <p>Total</p>
+              <p class="cart-total-price">{{ cartTotal | currency }}</p>
+            </div>
+            <div class="cart-bottom">
+              <h6>Continue Shopping</h6>
+              <button class="btn btn-black" @click="checkout">Checkout</button>
             </div>
           </div>
-          <hr>
-          <div class="cart-total">
-            <p>Total</p>
-            <p class="cart-total-price">{{ cartTotal | currency }}</p>
-          </div>
-          <div class="cart-bottom">
-            <h6>Continue Shopping</h6>
-            <button class="btn btn-black">Checkout</button>
-          </div>
+          <p v-else>Your cart is currently empty.</p>
         </div>
-         <p v-else>Your cart is currently empty.</p>
       </div>
 
     </div>
@@ -85,195 +96,197 @@
 </template>
 
 <script>
-//import stylesheets
-import "./scss/main.scss";
-//import components
+  //import stylesheets
+  import "./scss/main.scss";
+  //import components
 
-export default {
-  name: "app",
-  data() {
-    return {
-      isShowingCart: false,
+  export default {
+    name: "app",
+    data() {
+      return {
+        isShowingCart: false,
+        showModal: false,
+        cart: {
+          items: []
+        },
+        products: [{
+            id: 1,
+            name: "Food",
+            avatar: "../src/assets/FOOD.jpg",
+            description: "Available in size 12, 13, 14",
+            price: 120000,
 
-      cart: {
-        items: []
+          },
+          {
+            id: 2,
+            name: "Jeans",
+            avatar: "../src/assets/jeans.jpg",
+            description: "Available in size 12, 13, 14",
+            price: 120000,
+
+          },
+          {
+            id: 3,
+            name: "lens",
+            avatar: "../src/assets/lens.jpg",
+            description: "Available in size 12, 13, 14",
+            price: 120000,
+
+          },
+
+          {
+            id: 4,
+            name: "makeup",
+            avatar: "../src/assets/makeup.jpg",
+            description: "Available in size 12, 13, 14",
+            price: 120000,
+
+          },
+          {
+            id: 5,
+            name: "Truck",
+            avatar: "../src/assets/truck.jpg",
+            description: "Available in size 12, 13, 14",
+            price: 120000,
+
+          },
+          {
+            id: 6,
+            name: "Tshirt",
+            avatar: "../src/assets/chucks.png",
+            description: "Available in size 12, 13, 14",
+            price: 120000,
+
+          },
+          {
+            id: 7,
+            name: "Camera",
+            avatar: "../src/assets/camera.jpg",
+            description: "Available in size 12, 13, 14",
+            price: 120000,
+
+          },
+          {
+            id: 8,
+            name: "Tissot",
+            avatar: "../src/assets/tissot.jpg",
+            description: "Available in size 12, 13, 14",
+            price: 120000,
+
+          },
+          {
+            id: 9,
+            name: "Apple",
+            avatar: "../src/assets/apple.jpg",
+            description: "Available in size 12, 13, 14",
+            price: 120000,
+
+          },
+          {
+            id: 10,
+            name: "Tshirt",
+            avatar: "../src/assets/tshirt.jpg",
+            description: "Available in size 12, 13, 14",
+            price: 120000,
+
+          }
+        ]
+      };
+    },
+    methods: {
+
+      //add product to cart
+      addProductToCart: function (product) {
+        var cartItem = this.getCartItem(product);
+
+        if (cartItem != null) {
+          cartItem.quantity++;
+        } else {
+          this.cart.items.push({
+            product: product,
+            quantity: 1
+          });
+        }
+
+
       },
-      products: [
-        {
-          id: 1,
-          name: "Tshirt",
-          avatar: "../src/assets/tshirt.jpg",
-          description: "Available in size 12, 13, 14",
-          price: 120000,
-          
-        },
-        {
-          id: 2,
-          name: "Tshirt",
-          avatar: "../src/assets/tshirt.jpg",
-          description: "Available in size 12, 13, 14",
-          price: 120000,
-          
-        },
-        {
-          id: 3,
-          name: "Tshirt",
-          avatar: "../src/assets/tshirt.jpg",
-          description: "Available in size 12, 13, 14",
-          price: 120000,
-          
-        },
+      //remove product to cart
+      removeProductFromCart: function (product) {
+        var cartItem = this.getCartItem(product);
 
-        {
-          id: 4,
-          name: "Tshirt",
-          avatar: "../src/assets/tshirt.jpg",
-          description: "Available in size 12, 13, 14",
-          price: 120000,
+        /** if (cartItem != null) {
+          this.cart.items.splice(product);
           
-        },
-        {
-          id: 5,
-          name: "Tshirt",
-          avatar: "../src/assets/chucks.png",
-          description: "Available in size 12, 13, 14",
-          price: 120000,
-          
-        },
-        {
-          id: 6,
-          name: "Tshirt",
-          avatar: "../src/assets/chucks.png",
-          description: "Available in size 12, 13, 14",
-          price: 120000,
-          
-        },
-        {
-          id: 7,
-          name: "Tshirt",
-          avatar: "../src/assets/tshirt.jpg",
-          description: "Available in size 12, 13, 14",
-          price: 120000,
-          
-        },
-        {
-          id: 8,
-          name: "Tshirt",
-          avatar: "../src/assets/tshirt.jpg",
-          description: "Available in size 12, 13, 14",
-          price: 120000,
-        
-        },
-        {
-          id: 9,
-          name: "Tshirt",
-          avatar: "../src/assets/tshirt.jpg",
-          description: "Available in size 12, 13, 14",
-          price: 120000,
-          
-        },
-        {
-          id: 10,
-          name: "Tshirt",
-          avatar: "../src/assets/tshirt.jpg",
-          description: "Available in size 12, 13, 14",
-          price: 120000,
-         
-        }
-      ]
-    };
-  },
-  methods: {
-    addProductToCart: function(product) {
-      var cartItem = this.getCartItem(product);
+        } else {
+          cartItem.quantity == 0;
+        } */
+        this.cart.items.splice(product);
 
-      if (cartItem != null) {
+
+      },
+      //increase the quantity of cart item
+      increaseQuantity: function (cartItem) {
+
         cartItem.quantity++;
-      } else {
-        this.cart.items.push({
-          product: product,
-          quantity: 1
-        });
-      }
+      },
+      //decrease the quantity of cart item
+      decreaseQuantity: function (cartItem) {
+        cartItem.quantity--;
+        cartItem.product.inStock++;
 
-      
-    },
-    removeProductToCart: function(product) {
-      var cartItem = this.getCartItem(product);
-
-      if (cartItem = null) {
-        cartItem.quantity == 0;
-      } else {
-        this.cart.items.splice({
-          product: product,
-          quantity: 1
-        });
-      }
-
-      
-    },
-    increaseQuantity: function(cartItem) {
-      
-      cartItem.quantity++;
-    },
-
-    decreaseQuantity: function(cartItem) {
-      cartItem.quantity--;
-      cartItem.product.inStock++;
-
-      if (cartItem.quantity == 0) {
-        this.removeItemFromCart(cartItem);
-      }
-    },
-
-    removeItemFromCart: function(cartItem) {
-      var index = this.cart.items.indexOf(cartItem);
-
-      if (index !== -1) {
-        this.cart.items.splice(index, 1);
-      }
-    },
-
-    checkout: function() {
-      if (confirm("Are you sure that you want to purchase these products?")) {
-        this.cart.items.forEach(function(item) {
-          item.product.inStock += item.quantity;
-        });
-
-        this.cart.items = [];
-      }
-    },
-
-    getCartItem: function(product) {
-      for (var i = 0; i < this.cart.items.length; i++) {
-        if (this.cart.items[i].product.id === product.id) {
-          return this.cart.items[i];
+        if (cartItem.quantity == 0) {
+          this.removeItemFromCart(cartItem);
         }
+      },
+
+      removeItemFromCart: function (cartItem) {
+        var index = this.cart.items.indexOf(cartItem);
+
+        if (index !== -1) {
+          this.cart.items.splice(index, 1);
+        }
+      },
+
+      /**  checkout: function () {
+        this.$http.post('http://www.mocky.io/v2/5be477442f00004900d9f521', {
+          product = this.cart.items;
+
+        });
+        this.cart.items = [];
+      },*/
+      //get cart item
+      getCartItem: function (product) {
+        for (var i = 0; i < this.cart.items.length; i++) {
+          if (this.cart.items[i].product.id === product.id) {
+            return this.cart.items[i];
+          }
+        }
+
+        return null;
+      },
+
+    },
+
+    computed: {
+
+
+      cartTotal: function () {
+        var total = 0;
+
+        this.cart.items.forEach(function (item) {
+          total += item.quantity * item.product.price;
+        });
+
+        return total;
       }
-
-      return null;
+    },
+    filters: {
+      currency: function (value) {
+        return "₦" + parseFloat(value);
+      }
     }
-  },
+  };
 
-  computed: {
-    
-
-    cartTotal: function() {
-      var total = 0;
-
-      this.cart.items.forEach(function(item) {
-        total += item.quantity * item.product.price;
-      });
-
-      return total;
-    }
-  },
-  filters: {
-    currency: function(value) {
-      return "₦" + parseFloat(value);
-    }
-  }
-};
 </script>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
 
-
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
